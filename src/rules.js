685547a68d5d63a155bfa0e71f9db74b4473ebf5
julio.rezende <juliorezende@bemol.com.br>
2021-03-31 23:30:23 -0400

    Add: Fix alert sucess and warning

--- src/rules.js
+++ src/rules.js
@@ -1,3 +1,4 @@
+
 (function() {
   function validEmail(email) {
     var re = /^([\w-]+(?:\.[\w-]+)*)@((?:[\w-]+\.)*\w[\w-]{0,66})\.([a-z]{2,6}(?:\.[a-z]{2})?)$/i;
@@ -103,6 +104,32 @@
       forms[i].addEventListener("submit", handleFormSubmit, false);
     }
   };
+ 
+  $("#submit").click(function(){
+    var name = $("#name").val();
+    var email = $("#email").val();
+    var message = $("#message").val();
+  
+    if(name == '' || email == '' || message == ''){
+      swal({
+        title: "Opas!",
+        text: "Precisamos que todos os campos sejam preenchidos para enviarmos sua mensagem!",
+        icon: "warning",
+        button: "fechar",
+      });
+      return false
+    } else{
+
+      swal({
+        title: "Mensagem enviada com sucesso!",
+        text: "Agradecemos sua mensagem, vamos contatar você assim que possível pelo seu e-mail informado!",
+        icon: "success",
+        button: "fechar",
+      });
+    }
+  })
+  
+
   document.addEventListener("DOMContentLoaded", loaded, false);
 
   function disableAllButtons(form) {
